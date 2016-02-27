da93ff57abd419f7a88e589c51259b5918b97a8e
tiezhu-lee <lk0219@foxmail.com>
2016-02-27 09:20:37 +0800

    1001

--- 1001.cpp
+++ 1001.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int sum = a+b;
+	int t = abs(sum);
+	stringstream buf;
+	string tmp, res("");
+	if(sum < 0) res += '-';
+	buf << t;
+	buf >> tmp;
+	if(tmp.length() >3) {
+		int rmd = tmp.length() %3;
+		res += tmp.substr(0, rmd);
+		if(rmd != 0) res += ',';
+		for(int i = rmd ; i < tmp.length(); i += 3)
+		{
+			res += tmp.substr(i, 3);
+			if(i != tmp.length()-3) res += ',';
+		}
+	}
+	else res += tmp;
+
+	cout << res << endl;
+	return 0;
+	
+}
